d4ec792c7dc6aeb0575f2b6787c2aecf16919dc0
gcy <howareyou_mm@163.com>
2024-05-30 11:05:29 +0800

    重新开始

--- TaiJi/ProtoBuffer/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-//#include "cxx/any.pb.h"
-int main()
-{
-	return 0;
-}
--- TaiJi/SwigCplusplus/gencsBySwig.bat
+++
@@ -1,3 +0,0 @@
-d:
-cd .\swigwin-3.0.12\
-.\swig.exe -c++ -csharp E:\github\huashan\TaiJi\SwigCplusplus\CPlusPlusClass.idl
